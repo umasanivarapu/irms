11c94c1c5a69c83bf5662059c0c8e09b9b24a1a1
umasanivarapu <35694339+umasanivarapu@users.noreply.github.com>
2020-05-26 14:18:49 +0530

    Sidebar

--- js/userhome.js
+++ js/userhome.js
@@ -1,6 +1,9 @@
-$(document).ready(function () {
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
-        $(this).toggleClass('active');
-    });
-});
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
