4e36a7fa886ebad8cbe52f96a2b855896c63cfec
umasanivarapu <cs16btech11033@iith.ac.in>
2020-05-28 13:08:40 +0530

    asdfg

--- nodeserver.js
+++ nodeserver.js
@@ -3,24 +3,27 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
+var bodyParser = require('body-parser');
 
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 router.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/html/base.html'));
   //__dirname : It will resolve to your project folder.
 });
-
+app.post('/userlogin', function(req, res){
+	console.log('email: ' + req.body.email);
+	console.log('Password: ' + req.body.password);
+	 res.sendFile(path.join(__dirname+'/html/userhome.html'));
+});
 //add the router
 app.use(express.static(__dirname + '/html'));
-//Store all HTML files in view folder.
-app.use(express.static(__dirname + '/css'));
-//Store all JS and CSS in Scripts folder.
-app.use(express.static(__dirname + '/js'));
-//Store all JS and CSS in Scripts folder.
 app.use(express.static(__dirname + '/Images'));
-
-app.use('/', router);
+app.use(express.static(__dirname + '/css'));
+app.use(express.static(__dirname + '/js'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/',router)
 app.listen(process.env.port || 3000);
 
 console.log('Running at Port 3000');
